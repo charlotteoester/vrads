4ff85c6ca46054ca4093daef09e54cc4dc8584f9
idapr <idap.rasmussen@gmail.com>
2024-05-22 01:06:00 +0200

    Tilføjede JS som giver knap ny farve, når den er aktiv

--- script.js
+++ script.js
@@ -37,3 +37,37 @@ const dragStop = () => {
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mouseup", dragStop);
+
+
+
+//Der er ikke nogen aktive knapper på det tidspunkt, hvor koden kører:
+let activeButton = null;
+
+// VARIABLER
+// Henter alle knapper med class "hjerterytme-btn" og laver en fælles variabel kaldet puls_buttons
+const puls_buttons = document.querySelectorAll('.hjerterytme-btn'); 
+
+// Funktion til at håndtere knap klik
+function handleButtonClick(btn) {
+    let activeButton = document.querySelector('.active'); // Vi bruger document.querySelector('.active') for at finde den knap, der i forvejen har 'active' class
+
+    // Hvis en knap allerede er blevet klikket på og har fået "active" class, skal "active"-class fjernes fra den knap
+    if (activeButton) {
+        activeButton.classList.remove('active');
+        activeButton.style.backgroundColor = ""; // Opdater baggrundsfarve for den aktive knap
+    }
+
+    // Den knap, som klikkes på, får tilføjet "active" class
+    btn.classList.add('active');
+    btn.style.backgroundColor = "#811F25"; // Når en knap er aktiv, dvs. har fået class "active", skal baggrundsfarven ændres fra gennemsigtig til en mørk rød
+    
+}
+
+// Vi tilføjer en eventlistener til hver knap i puls_buttons, som starter funktionen "handleButtonClick"
+puls_buttons.forEach(function(btn) { 
+    btn.addEventListener('click', function() {
+        handleButtonClick(btn);
+    });
+});
+
+// Kilde: For at lave denne kode, brugte vi ChatGPT og ligeledes hjælp fra ChatGPT til at forklare det, vi ikke kunne gennemskue på egen hånd 
